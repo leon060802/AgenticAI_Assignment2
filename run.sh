d53c039508e687a3c098cd6a904f3fc7b6fe4614
BlackWaterGua <watermalon394@gmail.com>
2025-02-12 17:23:34 +0800

    [Init] Poetry Env

--- run.sh
+++ run.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-nohup python -u run.py \
-    --test_file ./data/tasks_test.jsonl \
-    --api_key YOUR_OPENAI_API_KEY \
-    --headless \
-    --max_iter 15 \
-    --max_attached_imgs 3 \
-    --temperature 1 \
-    --fix_box_color \
-    --seed 42 > test_tasks.log &
+python -u run.py --test_file ./data/tasks_test.jsonl --api_key YOUR_API_KEY --headless --max_iter 15 --max_attached_imgs 3 --temperature 1 --fix_box_color --api_model gpt-4o --seed 42
